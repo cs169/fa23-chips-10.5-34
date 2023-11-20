f81bfbc6e8fa7648cdaa14c24fc3c1588c939c04
Abigail Delgado <abigaildelgado@berkeley.edu>
2023-11-20 03:38:45 +0000

    updated rep controller to show

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 class RepresentativesController < ApplicationController
+  before_action :set_representative
+
   def index
     @representatives = Representative.all
+  end
+
+  def show
+    # used other news_items controller as ref
+    # @representative var alr set by before_action :set_representative
+  end
+  
+  private
+  def set_representative
+    @representative = Representative.find(
+      params[:representative_id]
+    )
   end
 end
