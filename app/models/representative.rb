f33ea78ee3636ba3f2f43fe2ef39ff385d4a8c0a
Evan Campbell-Widmer <evancampbell@berkeley.edu>
2023-11-21 16:33:38 +0000

    [backend] I used the api notation for the newly added aspects of the representative information hopefully it works out the spec is pretty vague

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -9,16 +9,22 @@ class Representative < ApplicationRecord
     rep_info.officials.each_with_index do |official, index|
       ocdid_temp = ''
       title_temp = ''
+      address_temp = ''
+      party_temp = ''
+      photo_temp = ''
 
       rep_info.offices.each do |office|
         if office.official_indices.include? index
           title_temp = office.name
           ocdid_temp = office.division_id
+          address_temp = office.address
+          party_temp = office.party
+          photo_temp = office.photoUrl
         end
       end
 
       rep = Representative.create!({ name: official.name, ocdid: ocdid_temp,
-          title: title_temp })
+          title: title_temp, address: address_temp, party: party_temp, photo: photo_temp})
       reps.push(rep)
     end
 
