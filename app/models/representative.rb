7a06ac77104767df19abc59e7d98207402a7e854
Evan Campbell-Widmer <evancampbell@berkeley.edu>
2023-11-27 22:54:12 +0000

    fixed the representative model

--- app/models/representative.rb
+++ app/models/representative.rb
@@ -14,24 +14,39 @@ class Representative < ApplicationRecord
           ocdid_temp = office.division_id
         end
       end
+      address_temp = official.address
       if Representative.find_by(name: official.name).nil?
-        address_temp = official.address
         rep = create_rep(address_temp, title_temp, ocdid_temp, official)
       else
-        address_temp = official.address
         rep = Representative.find_by(name: official.name)
-        rep.update(
-          ocdid: ocdid_temp,
-          title: title_temp,
-          line1: address_temp['line1'],
-          line2: address_temp['line2'],
-          line3: address_temp['line3'],
-          city:  address_temp['city'],
-          state: address_temp['state'],
-          zip:   address_temp['zip'],
-          party: official.party,
-          photo: official.photo_url
-        )
+        if address_temp.nil?
+          address_temp = ''
+          rep.update(
+            ocdid: ocdid_temp,
+            title: title_temp,
+            line1: address_temp,
+            line2: address_temp,
+            line3: address_temp,
+            city:  address_temp,
+            state: address_temp,
+            zip:   address_temp,
+            party: official.party,
+            photo: official.photo_url
+          )
+        else
+          rep.update(
+            ocdid: ocdid_temp,
+            title: title_temp,
+            line1: address_temp[0].line1,
+            line2: address_temp[0].line2,
+            line3: address_temp[0].line3,
+            city:  address_temp[0].city,
+            state: address_temp[0].state,
+            zip:   address_temp[0].zip,
+            party: official.party,
+            photo: official.photo_url
+          )
+        end
       end
       reps.push(rep)
     end
@@ -39,21 +54,20 @@ class Representative < ApplicationRecord
   end
 
   def create_rep(address, title, ocd, official)
-    rep = Representative.create!(
+    Representative.create!(
       {
         name:  official.name,
         ocdid: ocd,
         title: title,
-        line1: address.line1,
-        line2: address.line2,
-        line3: address.line3,
-        city:  address.city,
-        state: address.state,
-        zip:   address.zip,
+        line1: address[0].line1,
+        line2: address[0].line2,
+        line3: address[0].line3,
+        city:  address[0].city,
+        state: address[0].state,
+        zip:   address[0].zip,
         party: official.party,
         photo: official.photo_url
       }
     )
-    rep
   end
 end
