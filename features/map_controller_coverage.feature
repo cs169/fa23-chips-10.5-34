7380a0d694fc4057707df78fd81655dcae251bad
Genna Gams <ggams@berkeley.edu>
2023-11-28 07:36:51 +0000

    more progress

--- features/map_controller_coverage.feature
+++ features/map_controller_coverage.feature
@@ -6,6 +6,6 @@ Feature: Interacting with the map
 
 Scenario: I want to see all counties in a state
   Given I am on the homepage
-  When I click on the state California
+  When I click on the state CA
   When I click Counties in California
   Then I should see 58 counties
--- features/step_definitions/map_steps.rb
+++ features/step_definitions/map_steps.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 When /I click on the state (.*)/ do |state|
-  visit path_to('state')
+  redirect_to '/login'
 end
 
 When /I click Counties in (.*)/ do |state|
-  puts state
+  puts page.body
 end
 
 Then /I should see (.*) counties/ do |num|
