60391229247d93edfb156921c4f9fe886fcfc363
Evan Campbell-Widmer <evancampbell@berkeley.edu>
2023-11-30 03:02:26 +0000

    Made the campaign finances model and controller

---
+++ app/controllers/campaign_finance_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class CampaignFinancesController < ApplicationController
+
+  def index
+    @campaign_finances = CampaignFinances.all
+  end
+
+  def search
+    cycle, category = params[:cycle, :category]
+    api_key = ENV['PROPUBLICA_API_KEY']
+    service = CampaignFinances.initialize(api_key)
+    result = service.propublica_api_to_candidates(cycle, category)
---
+++ app/models/campaign_finances.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class CampaignFinances < ApplicationRecord
+  include HTTParty
+  base_uri 'https://api.propublica.org'
+
+  def initialize(api_key)
+    @api_key = api_key
+    @headers = {
+      'X-API-Key' => @api_key,
+      'Content-Type' => 'application/json'
+    }
+  end
+
+  def self.propublica_api_to_representatives(cycle, category)
+    response = class.get('/campaign-finance/v1/#{cycle}/candidates/leaders/#{category}.json', headers: @headers)
+    JSON.parse(response.body)['results'].each_with_index do |result, index|
+      candidates.push(result.name)
+    end
+    candidates
+  end
+
+      
+    
